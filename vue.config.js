bf81a9b0050f644e53935f9c5d1558c34bb28344
李朋飞_启明 <lpfa0402@163.com>
2020-09-01 10:24:42 +0800

    commit

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+// Vue.config.js
+module.exports = {
+  // publicPath: '',
+  /**
+   *  设置项目部署的基础路径
+   *  默认不加地址为 http://localhost:8081/
+   *  添加publicPath: 'testPath' 后打包地址为 http://localhost:8081/testPath
+   *  例如，如果你的应用被部署在 https://www.my-app.com/my-app/，则设置 publicPath 为 /my-app/
+   */
+  outputDir: 'distAA'
+}
